52bd211f07d70e473ffc1c54fd3bf36bf1dd7ca3
Sean Abbas <sean.abbas33@gmail.com>
2023-02-10 15:35:55 -0500

    kelly's subpar coding

--- practice-for-ch-sql-movie-buff-long-practice-main/movie_buff/01_queries.rb
+++ practice-for-ch-sql-movie-buff-long-practice-main/movie_buff/01_queries.rb
@@ -59,6 +59,8 @@ def directed_by_one_of(them)
   
   # Note: Directors appear in the 'actors' table.
 
+  Movie.joins(:director).where(actors: {name: them}).select(:id, :title)
+
 end
 
 def movie_names_before_1940
@@ -72,5 +74,7 @@ def movie_names_before_1940
   # improve performance for larger queries.
   #
   # Use pluck to find the title of all movies made before 1940.
+
+  Movie.where("yr < 1940").pluck(:title)
 
 end
