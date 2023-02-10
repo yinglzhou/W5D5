935daaaebc8b28b2f5c4760b21cb0af651c1558b
Kelly Zhou <yinglinzhou0@gmail.com>
2023-02-10 17:32:37 -0500

    final push

--- practice-for-ch-sql-movie-buff-long-practice-main/movie_buff/02_queries.rb
+++ practice-for-ch-sql-movie-buff-long-practice-main/movie_buff/02_queries.rb
@@ -1,11 +1,14 @@
 def eighties_b_movies
   # List all the movies from 1980-1989 with scores falling between 3 and 5
   # (inclusive). Show the id, title, year, and score.
-  
+
+  Movie.where(score: 3..5).select(:id, :title, :yr, :score).where(yr: 1980..1989)
 end
 
 def bad_years
   # List the years in which no movie with a rating above 8 was released.
+
+  Movie.group(:yr).having("MAX(score) <= 8").pluck(:yr)
   
 end
 
